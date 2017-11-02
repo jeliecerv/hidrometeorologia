d2bac145225956b48de51639915a16059751645d
Jorge Eliecer <jeliecerv@gmail.com>
2017-11-02 18:45:50 -0500

    Primer commit Proyecto

---
+++ Hidrometeorologia/app/src/main/java/com/example/jelie/hidrometeorologia/MainHidrometoorologia.java
@@ -0,0 +1,13 @@
+package com.example.jelie.hidrometeorologia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHidrometoorologia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hidrometoorologia);
+    }
+}
