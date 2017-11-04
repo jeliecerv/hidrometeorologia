8cdee5993ed38f08ea3be0e5b91972a2e9e84508
yennyPedraza <33361859+yennyPedraza@users.noreply.github.com>
2017-11-04 18:25:20 -0500

    holholholaa

--- Hidrometeorologia/app/src/main/java/com/example/jelie/hidrometeorologia/PagosFragment.java
+++ Hidrometeorologia/app/src/main/java/com/example/jelie/hidrometeorologia/PagosFragment.java
@@ -1,13 +1,16 @@
 package com.example.jelie.hidrometeorologia;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 /**
@@ -18,21 +21,45 @@ public class PagosFragment extends Fragment {
 
     private ListView listView;
 
+    Cursor pagos;
+    HMoPagosAdapter dbHelper;
+
     public PagosFragment() {
         // Required empty public constructor
     }
 
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        dbHelper = new HMoPagosAdapter(getActivity());
+        dbHelper.open();
+    }
+
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_pagos, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_pagos, container, false);
         listView = (ListView) view.findViewById(R.id.pagosList);
-        
+        refresh();
         return view;
     }
+
+    private void refresh() {
+        pagos = dbHelper.fetchAllPagos();
+        pagos.moveToFirst();
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(getActivity(),
+                R.layout.pagos_row,
+                pagos,
+                new String[]{HMoPagosAdapter.KEY_MES, HMoPagosAdapter.KEY_DIASREPORTE, HMoPagosAdapter.KEY_VALORREPORTE,HMoPagosAdapter.KEY_TOTALPAGADO, HMoPagosAdapter.KEY_DEUDA},
+                     new int[]{R.id.mes, R.id.diasreporte, R.id.valorreporte, R.id.totalpagado, R.id.deuda},0);
+        listView.setAdapter(cursorAdapter);}
+
 
     public void listPagos() {
 //        ArrayAdapter<String> itemsAdapter =
